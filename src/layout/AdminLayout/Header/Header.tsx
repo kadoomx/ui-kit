5cc80c7f256f475014fa9589eb757f9cc2f65c20
Osvaldo Garcia <aoga88@gmail.com>
2023-02-14 21:47:12 -0600

    Initial commit

---
+++ src/layout/AdminLayout/Header/Header.tsx
@@ -0,0 +1,46 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
+import HeaderProfileNav from '@/layout/AdminLayout/Header/HeaderProfileNav'
+import { Button, Container } from 'react-bootstrap'
+
+type HeaderProps = {
+  toggleSidebar: () => void;
+  toggleSidebarMd: () => void;
+}
+
+export default function Header(props: HeaderProps) {
+  const { toggleSidebar, toggleSidebarMd } = props
+
+  return (
+    <header className="header sticky-top mb-4 p-2 border-bottom">
+      <Container fluid className="header-navbar d-flex align-items-center">
+        <Button
+          variant="link"
+          className="header-toggler d-md-none px-md-0 me-md-3 rounded-0 shadow-none"
+          type="button"
+          onClick={toggleSidebar}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </Button>
+        <Button
+          variant="link"
+          className="header-toggler d-none d-md-inline-block px-md-0 me-md-3 rounded-0 shadow-none"
+          type="button"
+          onClick={toggleSidebarMd}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </Button>
+        <Link href="/" className="header-brand d-md-none">
+          <img width="118" height="46" src="/assets/logo.svg" />
+        </Link>
+        <div className="header-nav ms-auto">
+          
+        </div>
+        <div className="header-nav ms-2">
+          <HeaderProfileNav />
+        </div>
+      </Container>
+    </header>
+  )
+}
